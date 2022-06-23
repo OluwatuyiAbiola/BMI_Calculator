2416f15441fd705b538c1dd2867085b2cb52a1ad
Oluwatuyi Abiola <abiolaoluwatuyi@gmail.com>
2022-06-23 02:38:51 +0100

    Started the structure in our input page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -14,9 +14,68 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: const Text('BMI Calculator'),
       ),
-      body: const Center(
-        child: Text('Calculator Body'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child:Row(
+              children: const <Widget>[
+                Expanded(
+                  child:ReusableCard(
+                    colour: Color(0xfff1d1e33),
+                   ),
+                ),
+                Expanded(
+                  child:ReusableCard(
+                    colour: Color(0xfff1d1e33)
+                  ),
+                ),
+              ],
+            ),
+            ),
+          const Expanded(
+            child:ReusableCard(
+              colour: Color(0xfff1d1e33)
+            ), 
+            ),
+          Expanded(
+            child:Row(
+              children: const <Widget>[
+                Expanded(
+                  child:ReusableCard(
+                    colour: Color(0xfff1d1e33)
+                  ),
+                  ),
+                Expanded(
+                  child:ReusableCard(
+                    colour: Color(0xfff1d1e33)
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+
       ),
+      
     );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  
+
+  const ReusableCard({required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: colour
+            ),
+          );
   }
 }
