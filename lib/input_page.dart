e20e442ce0b39957ed82934e2209ba335947f9ed
Oluwatuyi Abiola <abiolaoluwatuyi@gmail.com>
2022-06-24 02:16:11 +0100

    made icons interactive and used gesturedetector

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -10,6 +10,7 @@ import 'Reusable_card.dart';
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xff1d1e33);
 const bottomContainerColour = Color(0xffeb1555);
+const inactiveCardColour =  Color(0xff111328);
 
 class InputPage extends StatefulWidget {
   
@@ -18,6 +19,29 @@ class InputPage extends StatefulWidget {
   State<InputPage> createState() => _InputPageState();
 }
 
+//setting a variable for the active color
+Color maleCardColor = inactiveCardColour;
+Color femaleCardColor = inactiveCardColour;
+//create a function (conditional) to show the color when either a male card or female card is pressed
+void updateColour (int gender){
+  //1 == male and 2 ==  female
+  if (gender == 1){
+    if(maleCardColor == inactiveCardColour){
+      maleCardColor = activeCardColour;
+      femaleCardColor = inactiveCardColour;
+    } else {
+      maleCardColor = inactiveCardColour;
+    }
+  }
+  if (gender == 2){
+    if(femaleCardColor == inactiveCardColour){
+      femaleCardColor = activeCardColour;
+      maleCardColor = inactiveCardColour;
+    } else {
+      femaleCardColor = inactiveCardColour;
+    }
+  }
+} 
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
@@ -32,24 +56,38 @@ class _InputPageState extends State<InputPage> {
               // ignore: prefer_const_literals_to_create_immutables
               children: <Widget>[
                 Expanded(
-                  child:ReusableCard(
-                    colour: activeCardColour,
-                    //extracted the column widget
-                    cardChild: IconContent(
-                      icon: FontAwesomeIcons.mars,
-                      label: 'MALE',
-                    ),
-                   ),
+                  child:GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        updateColour(1);
+                      });
+                    },
+                    child: ReusableCard(
+                      colour: maleCardColor,
+                      //extracted the column widget
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: 'MALE',
+                      ),
+                     ),
+                  ),
                 ),
                 Expanded(
-                  child:ReusableCard(
-                    colour: activeCardColour,
-                    //note the reusable class has two properties one for color and the other for cardchild
-                    //cardChild has a class named icon content
-                    //icon content has two properties icon and label
-                     cardChild: IconContent(
-                      icon: FontAwesomeIcons.venus,
-                      label: 'FEMALE',
+                  child:GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColour(2);
+                      });
+                    },
+                    child: ReusableCard(
+                      colour: femaleCardColor,
+                      //note the reusable class has two properties one for color and the other for cardchild
+                      //cardChild has a class named icon content
+                      //icon content has two properties icon and label
+                       cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE',
+                      ),
                     ),
                   ),
                 ),
